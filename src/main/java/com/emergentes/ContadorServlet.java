6f18282013223d0b3520b9c67a075e8e238ab531
Franklin B. Mamani M <benzfbm@gmail.com>
2022-10-12 18:31:34 -0400

    Primer commit

---
+++ src/main/java/com/emergentes/ContadorServlet.java
@@ -0,0 +1,55 @@
+
+package com.emergentes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "ContadorServlet", urlPatterns = {"/ContadorServlet"})
+public class ContadorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+        
+      int contador = 0;
+      Cookie[] cukis = request.getCookies();
+      if (cukis != null){
+          
+          for(Cookie c : cukis){
+              if (c.getName().equals("visitas")) {
+                  contador= Integer.parseInt(c.getValue());
+              }
+          }
+      }
+      contador ++;
+      Cookie c = new Cookie ("visitas",Integer.toString(contador));
+      //Establece el tiempo de la cookie en segumdos
+      c.setMaxAge(3600);
+      response.addCookie(c);
+      //Generar contenido a partit del Servlet
+      response.setContentType("text/html");
+      
+      PrintWriter out = response.getWriter();
+      out.print("Visitante No:" + contador);
+      
+        
+    }
+
+    
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+       
+    }
+
+   
+
+}
